b1ac6b139ee3477a539e35df5dd2c4216d33937c
Gary <kikz270@gmail.com>
2020-05-25 17:05:10 -0500

    added table

--- app.js
+++ app.js
@@ -38,16 +38,19 @@ var config = {
         }
     }
 };
-
-
-var myChart = new Chart(ctx, config);
-printBtn();
-
-
 var monthButtonArray = document.getElementById("monthButtonArray");
 var buttonLabels = ['January', 'Febuary', 'March', 'April', 'May', 
 'June', 'July', 'August', 'September', 'October', 
 'November', 'December'];
+var tableDiv = document.getElementById('dataTable');
+
+
+
+var myChart = new Chart(ctx, config);
+printBtn();
+createTable();
+
+
 
 function printBtn(){
     for (var i = 0; i < buttonLabels.length; i++){
@@ -58,5 +61,28 @@ function printBtn(){
     }
 }
 
+function createTable(){
+    
+    
+    var table = document.createElement('table');
+    table.border = 1;
 
+    var tableBody = document.createElement('tbody');
+    table.appendChild(tableBody);
+
+    for (var i = 0; i < 3; i++){
+        var tr = document.createElement('tr');
+        tableBody.appendChild(tr);
+
+        for(var j = 0; j < 2; j++){
+            var td = document.createElement('td');
+            td.width = '75';
+            td.appendChild(document.createTextNode("'Cell " + i + "," + j));
+            tr.appendChild(td);
+        
+        }
+    }
+    tableDiv.appendChild(table);
+    
+}
 
